683062cc642945f5da0fb71573d3e978bd7e142e
adrimrutra <amir@isd.dp.ua>
2019-06-05 16:59:03 +0300

    Logger

--- api/app.ts
+++ api/app.ts
@@ -3,18 +3,20 @@ import { NextFunction, Request, Response } from 'express';
 import * as bodyParser from 'body-parser';
 import { Router } from './core/router';
 import errorMiddleware from './middleware/error.middleware';
-import LoggerProvider from './providers/logger.provider';
-import * as morgan from 'morgan';
-import WinstonLogger from './config/winston.logger';
+import LoggerProvider from './middleware/logger.provider';
+
+
 import * as winston from 'winston';
-import { levels } from 'logform';
+
+
+
 
 const port = process.env.PORT || '4000';
 
 class App {
     public app: express.Application;
     public router = new Router();
-  //  public winstonLogger = new WinstonLogger();
+    public loggerProvider = new LoggerProvider();
 
 
 
@@ -24,37 +26,35 @@ class App {
     }
 
     private initializeMiddlewares() {
-         this.app.use(bodyParser.json());
+        this.app.use(bodyParser.json());
 
-         this.app.use(function(req, res, next) {
+        this.app.use(function(req, res, next) {
           res.header('Access-Control-Allow-Origin', '*');
           res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
           res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, x-access-token, Content-Type, Accept');
           next();
-         });
-         const logger = winston.createLogger({
-          format: winston.format.json(),
-          defaultMeta: { service: 'user-service' },
-          transports: [
-            //
-            // - Write to all logs with level `info` and below to `combined.log` 
-            // - Write all logs error (and below) to `error.log`.
-            //
-            new winston.transports.File({ filename: 'error.log', level: 'error' }),
-            new winston.transports.File({ filename: 'combined.log',  }),
-            new winston.transports.Console({ level: 'info'})
-          ]
         });
 
-        // logger.info('hello logger');
-
        // this.app.use(LoggerProvider.loggerMiddleware);
-       // this.app.use(morgan('combined'));
-       // this.app.use(morgan('combined', { stream: this.winstonLogger.logger.stream }));
-        this.app.use(morgan('combined', { stream: logger }));
+
+
+        // Log a message
+        // this.loggerMiddleware.logger.log({
+        //   message: 'Hello, Winston!',
+        //   level: 'info'
+        // });
+        // Log a message
+        //this.loggerMiddleware.logger.info('Hello, Winston!');
+        //this.loggerMiddleware.logger.error('Hello, Winston!');
+
+        this.app.use(this.loggerProvider.loggerMiddleware);
+
         this.app.use('/api', this.router.route());
 
-        logger.info('combined');
+
+
+
+
 
     }
 
@@ -70,6 +70,7 @@ class App {
     public listen() {
         this.app.listen(port, () => {
           console.log('App listening on the port ' + port);
+          //this.winstonLogger.logger.info('App listening on the port ' + port);
         });
     }
 }
